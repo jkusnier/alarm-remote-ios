0ce94b8137db05fb01d164e20f39ee5c4072d303
Jason Kusnier <jkusnier@gmail.com>
2014-12-30 08:42:08 -0500

    created tests for APIController

--- alarm-remote-iosTests/alarm_remote_iosTests.swift
+++ alarm-remote-iosTests/alarm_remote_iosTests.swift
@@ -11,26 +11,89 @@ import XCTest
 
 class alarm_remote_iosTests: XCTestCase {
     
+    let userName = "test"
+    let password = "encryptedpassword"
+    var accessToken: String?
+    var deviceId: String?
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+        
+        let api = APIController()
+        let expectation = expectationWithDescription("Get Access Token")
+        api.getAccessToken(userName, password: password, failure: nil, success: { accessToken in
+            self.accessToken = accessToken
+            expectation.fulfill()
+        })
+        
+        waitForExpectationsWithTimeout(10) { (error) in
+            XCTAssertNil(error)
+        }
+        
+        let expectation2 = expectationWithDescription("Get Device Id")
+        api.updateDevices(
+            accessToken: accessToken!,
+            success: { devices in
+                self.deviceId = devices.keys.first
+                expectation2.fulfill()
+        })
+        
+        waitForExpectationsWithTimeout(10) { (error) in
+            XCTAssertNil(error)
         }
     }
     
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testAuth() {
+        let api = APIController()
+        api.getAccessToken(self.userName, password: self.password,
+            failure: { error in
+                XCTAssertTrue(false)
+            },
+            success: { accessToken in
+                XCTAssertNotNil(accessToken, "Token is nill")
+                
+                api.testAccessToken(self.userName, accessToken: accessToken,
+                    failure: { error in
+                        XCTAssertTrue(false, "Token is invalid")
+                    },
+                    success: {
+                        XCTAssertTrue(true)
+                        self.accessToken = accessToken
+                })
+        })
+    }
+    
+    func testUpdateDevices() {
+        XCTAssertNotNil(self.accessToken, "Access Token Not Found")
+        
+        let api = APIController()
+        
+        api.updateDevices(
+            accessToken: self.accessToken!,
+            failure: { error in
+                XCTAssertTrue(false, "Error Retrieving Devices")
+            },
+            success: { devices in
+                XCTAssertTrue(devices.count > 0, "No Devices Found")
+        })
+    }
+    
+    func testGetDeviceAlarms() {
+        XCTAssertNotNil(self.accessToken, "Access Token Not Found")
+        XCTAssertNotNil(self.deviceId, "Device Id Not Found")
+        
+        let api = APIController()
+        
+        api.getDeviceAlarms(accessToken: self.accessToken, deviceId: self.deviceId,
+            failure: { error in
+                XCTAssertTrue(false, "Error Retrieving Alarms")
+            },
+            success: { alarms in
+                XCTAssertTrue(alarms.count > 0, "No Alarms Found")
+        })
+    }
 }
