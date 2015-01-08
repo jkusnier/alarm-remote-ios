77a464f83b95242d061eff3a6f83385641fed1e4
Jason Kusnier <jkusnier@gmail.com>
2015-01-08 18:27:07 -0500

    created controller for alarm editing

---
+++ alarm-remote-ios/EditAlarmTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EditAlarmTableViewController.swift
+//  alarm-remote-ios
+//
+//  Created by Jason Kusnier on 1/8/15.
+//  Copyright (c) 2015 Jason Kusnier. All rights reserved.
+//
+
+import UIKit
+
+class EditAlarmTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
