394683c4d13cf089b062abf11bed0d154fd1f423
Jason Kusnier <jkusnier@gmail.com>
2015-01-11 08:35:56 -0500

    read portion of day of weeks added to alarm edit

--- alarm-remote-ios/EditAlarmTableViewController.swift
+++ alarm-remote-ios/EditAlarmTableViewController.swift
@@ -10,7 +10,19 @@ import UIKit
 
 class EditAlarmTableViewController: UITableViewController {
 
-    var alarm: [String: AnyObject?]?
+    @IBOutlet weak var sundayCell: UITableViewCell!
+    @IBOutlet weak var mondayCell: UITableViewCell!
+    @IBOutlet weak var tuesdayCell: UITableViewCell!
+    @IBOutlet weak var wednesdayCell: UITableViewCell!
+    @IBOutlet weak var thursdayCell: UITableViewCell!
+    @IBOutlet weak var fridayCell: UITableViewCell!
+    @IBOutlet weak var saturdayCell: UITableViewCell!
+    
+    var alarm: [String: AnyObject?]? {
+        didSet {
+            self.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,5 +30,46 @@ class EditAlarmTableViewController: UITableViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.reloadData()
+        
+        return super.viewWillAppear(animated)
+    }
+    
+    private func reloadData() {
+        if self.isViewLoaded() {
+            if let dayOfWeek = alarm?["dayOfWeek"] as? [Int] {
+                self.sundayCell.accessoryType = UITableViewCellAccessoryType.None
+                self.mondayCell.accessoryType = UITableViewCellAccessoryType.None
+                self.tuesdayCell.accessoryType = UITableViewCellAccessoryType.None
+                self.wednesdayCell.accessoryType = UITableViewCellAccessoryType.None
+                self.thursdayCell.accessoryType = UITableViewCellAccessoryType.None
+                self.fridayCell.accessoryType = UITableViewCellAccessoryType.None
+                self.saturdayCell.accessoryType = UITableViewCellAccessoryType.None
+                
+                for day in dayOfWeek {
+                    switch day {
+                    case 1:
+                        self.sundayCell.accessoryType = UITableViewCellAccessoryType.Checkmark
+                    case 2:
+                        self.mondayCell.accessoryType = UITableViewCellAccessoryType.Checkmark
+                    case 3:
+                        self.tuesdayCell.accessoryType = UITableViewCellAccessoryType.Checkmark
+                    case 4:
+                        self.wednesdayCell.accessoryType = UITableViewCellAccessoryType.Checkmark
+                    case 5:
+                        self.thursdayCell.accessoryType = UITableViewCellAccessoryType.Checkmark
+                    case 6:
+                        self.fridayCell.accessoryType = UITableViewCellAccessoryType.Checkmark
+                    case 7:
+                        self.saturdayCell.accessoryType = UITableViewCellAccessoryType.Checkmark
+                    default:
+                        println("Invalid Day")
+                    }
+                }
+            }
+        }
     }
 }
