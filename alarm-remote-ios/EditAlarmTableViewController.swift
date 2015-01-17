dd52cd64d61f3b81db8b82522eb27849b6aae716
Jason Kusnier <jkusnier@gmail.com>
2015-01-17 07:16:17 -0500

    date picker starts with current alarm time

--- alarm-remote-ios/EditAlarmTableViewController.swift
+++ alarm-remote-ios/EditAlarmTableViewController.swift
@@ -100,6 +100,13 @@ class EditAlarmTableViewController: UITableViewController {
         var datePickerView  : UIDatePicker = UIDatePicker()
         datePickerView.datePickerMode = UIDatePickerMode.Time
         sender.inputView = datePickerView
+        
+        let dateStringFormatter = NSDateFormatter()
+        dateStringFormatter.dateFormat = "h:mm a"
+        dateStringFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+        let d = dateStringFormatter.dateFromString(self.timeText.text)
+        datePickerView.setDate(d!, animated: false)
+        
         datePickerView.addTarget(self, action: Selector("handleDatePicker:"), forControlEvents: UIControlEvents.ValueChanged)
         
     }
