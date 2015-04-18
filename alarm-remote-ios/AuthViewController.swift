ece5948394878096619c2fb3b3e98717fb794329
Jason Kusnier <jkusnier@gmail.com>
2015-04-18 08:52:36 -0400

    removed unused imports

--- alarm-remote-ios/AuthViewController.swift
+++ alarm-remote-ios/AuthViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import PKHUD
 
 class AuthViewController: UIViewController, UITextFieldDelegate {
 
--- alarm-remote-ios/MainViewController.swift
+++ alarm-remote-ios/MainViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import PKHUD
 
 class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
